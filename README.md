c69f219f891e6f98028025ae257fe8ddf0850b20
jonathan senior <jonosenior@gmail.com>
2017-11-07 14:48:32 +0100

    Added user instructions and a 'play again?' function

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # Tic Tac Toe Game
+This is a game of Noughts and Crosses (Tic-Tac-Toe to Americans) playable in the command line.  
+
+![Screenshot](tictactoescreenshot.png)
+
+It's a project from [The Odin Project](https://www.theodinproject.com/courses/ruby-programming/lessons/oop).
 
 
 ## Installation
 
 Open your terminal/command line. Navigate to the directory where you want this project to live. Type:
 ```
+$ git clone https://github.com/Jonosenior/tictactoe.git
+$ cd tictactoe
+$ ruby tictactoe.rb
 ```
-I'm excited to write my first working game in Ruby. I hope to better understand why object oriented (as opposed to procedural) programming is a useful way to organise code.
 
 ## Pre-project thoughts
 
+I'm excited to write my first working game in Ruby. I hope to better understand why object oriented (as opposed to procedural) programming is a useful way to organise code.
 
 ## Post-project thoughts
+
+I found the differe
+
+## Features to add
+  1. Screenshot of the game in this README.
+  3. AI so that you can play against the computer.
--- tictactoe.rb
+++ tictactoe.rb
@@ -10,7 +10,9 @@ class Board
   end
 
   def display_board
-    puts "#{@board[0]}\n#{@board[1]}\n#{@board[2]}"
+    print "     |     |     \n  #{@board[0][0]}  |  #{@board[0][1]}  |  #{@board[0][2]}\n"
+    print "_____|_____|_____\n     |     |   \n  #{@board[1][0]}  |  #{@board[1][1]}  |  #{@board[1][2]}  \n"
+    print "_____|_____|_____\n     |     |   \n  #{@board[2][0]}  |  #{@board[2][1]}  |  #{@board[2][2]}  \n     |     |     "
   end
 
   def place_move(marker, grid_number)
@@ -95,13 +97,17 @@ class Game
   end
 
   def ask_player_names
-    puts "Please give me your name Player 1"
+    puts "Let's play some Noughts and Crosses!"
+    puts "First, please give me your name Player 1"
     player_1_name = gets.chomp
     player_1 = Player.new(player_1_name, "X")
-    puts "Your name Player 2"
+    puts "And your name Player 2?"
     player_2_name = gets.chomp
     player_2 = Player.new(player_2_name, "O")
     @players.push(player_1, player_2)
+    puts "Lovely to meet you, #{player_1_name} and #{player_2_name}!"
+    puts "The rules are simple: choose where you want to play by selecting a number on the grid."
+    puts "The first player to reach three in a row wins."
   end
 
   def change_current_player
@@ -116,12 +122,11 @@ class Game
      new_turn
     end
     board.place_move(@current_player.marker, @move.to_i)
-    puts board.display_board
     status
   end
 
   def ask_user_for_move
-    puts "OK #{current_player.name}. Make your move."
+    puts "\nOK #{current_player.name}. Make your move."
     @move = gets.chomp
   end
 
@@ -137,17 +142,33 @@ class Game
   end
 
   def we_have_a_winner
-    puts "Congratulations #{current_player.name}!"
-    exit
+    puts "Congratulations #{current_player.name}! You smashed it!"
+    suggest_new_game
   end
 
   def everyone_loses
     puts "I'm afraid you're both losers, #{players[0].name} and #{players[1].name}."
+    suggest_new_game
+  end
+
+  def suggest_new_game
+    puts "Do you want to play again? (y/n)"
+    answer = gets.chomp
+    answer == "y" ? new_game : goodbye
+  end
+
+  def goodbye
+    puts "Great playing with you. Let's play again soon!"
     exit
   end
 
 end
 
 
-game = Game.new
-game.new_turn
+def new_game
+  puts "Starting game..."
+  game = Game.new
+  game.new_turn
+end
+
+new_game
