0d20ec26af1d3bd8d315fcb1571888269db17b56
jonathan senior <jonosenior@gmail.com>
2017-12-14 13:19:53 +0100

    Add early version of Rspec test suite

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe Game do
   subject(:tic) { Game.new }
-  subject.stub(:gets) { 1 }
+  subject.stub(:gets) {"Dan"}
 
-  describe ".ask_player_names"
-  it "asks user to input player names" do
+  describe ".ask_player_names" do
+    it "asks user to input player names" do
+      expect(:tic.ask_player_names).to eq("Dan")
+    end
 
   end
 
