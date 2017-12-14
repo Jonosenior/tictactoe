1c6e87d9ca6c74ffe8a1e044009df0e847de9493
jonathan senior <jonosenior@gmail.com>
2017-12-14 16:51:35 +0100

    Fix bug where no victory on final turn

--- lib/game.rb
+++ lib/game.rb
@@ -2,25 +2,40 @@
 # conducts the game.
 
 class Game
-  attr_reader :board, :players, :current_player
+  attr_reader :board, :players, :current_player, :move
 
   def initialize
     @board = Board.new
     @players = []
-    ask_player_names
-    @current_player = players[0]
   end
 
-  def ask_player_names
+  def set_current_player
+    @current_player = @players[0]
+  end
+
+  def update_players_array
+    @players << Player.new(@player_1_name, "X")
+    @players << Player.new(@player_2_name, "O")
+  end
+
+  def introduction
     puts "Let's play some Noughts and Crosses!"
+  end
+
+  def ask_player_1_name
     puts "First, please give me your name Player 1"
-    player_1_name = gets.chomp
-    player_1 = Player.new(player_1_name, "X")
+    @player_1_name = gets.chomp
+    #player_1 = Player.new(player_1_name, "X")
+  end
+
+  def ask_player_2_name
     puts "And your name Player 2?"
-    player_2_name = gets.chomp
-    player_2 = Player.new(player_2_name, "O")
-    @players.push(player_1, player_2)
-    puts "Lovely to meet you, #{player_1_name} and #{player_2_name}!"
+    @player_2_name = gets.chomp
+    #player_2 = Player.new(player_2_name, "O")
+  end
+
+  def print_instructions
+    puts "Lovely to meet you, #{@player_1_name} and #{@player_2_name}!"
     puts "The rules are simple: choose where you want to play by selecting a number on the grid."
     puts "The first player to reach three in a row wins."
   end
@@ -29,15 +44,15 @@ class Game
     @current_player = (@current_player == @players[0] ? @players[1] : @players[0])
   end
 
-  def new_turn
-    board.display_board
-    @move = ask_user_for_move
-    if !(board.is_move_legit?(@move))
-     puts "Make a proper move, moron"
-     new_turn
+  def verify_input
+    # board.display_board
+    # @move = ask_user_for_move
+    while !(board.is_move_legit?(@move))
+      puts "Make a proper move, moron"
+      @move = ask_user_for_move
     end
-    board.place_move(@current_player.marker, @move.to_i)
-    status
+    #board.place_move(@current_player.marker, @move.to_i)
+    #status
   end
 
   def ask_user_for_move
@@ -45,31 +60,29 @@ class Game
     @move = gets.chomp
   end
 
-  def status
-    if board.any_winning_lines?(@current_player)
-      we_have_a_winner
-    elsif board.is_it_a_draw?
-      everyone_loses
-    else
-      change_current_player
-      new_turn
-    end
-  end
+  # def status
+  #   if board.any_winning_lines?(@current_player)
+  #     we_have_a_winner
+  #   elsif board.is_it_a_draw?
+  #     everyone_loses
+  #   else
+  #     change_current_player
+  #     new_turn
+  #   end
+  # end
 
   def we_have_a_winner
-    puts "Congratulations #{current_player.name}! You smashed it!"
-    suggest_new_game
+    puts "\n\nCongratulations #{current_player.name}! You smashed it!"
   end
 
   def everyone_loses
-    puts "I'm afraid you're both losers, #{players[0].name} and #{players[1].name}."
-    suggest_new_game
+    puts "\n\nI'm afraid you're both losers, #{players[0].name} and #{players[1].name}."
   end
 
-  def suggest_new_game
+  def user_wants_new_game?
     puts "Do you want to play again? (y/n)"
-    answer = gets.chomp
-    answer == "y" ? new_game : goodbye
+    answer = gets.chomp.downcase
+    answer == "y" ? true : false
   end
 
   def goodbye
--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -2,10 +2,29 @@ require_relative 'game'
 require_relative 'board'
 require_relative 'player'
 
-def new_game
-  puts "Starting game..."
+def start_game
+  puts "Starting game...\n...\n...\n"
   game = Game.new
-  game.new_turn
+  board = game.board
+  game.introduction
+  game.ask_player_1_name
+  game.ask_player_2_name
+  game.update_players_array
+  game.print_instructions
+  game.set_current_player
+  loop do
+    board.display_board
+    game.ask_user_for_move
+    game.verify_input
+    board.place_move(game.current_player.marker, game.move.to_i)
+    if board.is_it_a_draw? || board.any_winning_lines?(game.current_player)
+      board.display_board
+      break
+    end
+    game.change_current_player
+  end
+  board.any_winning_lines?(game.current_player) ? game.we_have_a_winner : game.everyone_loses
+  game.user_wants_new_game? ? start_game : game.goodbye
 end
 
-new_game
+start_game
