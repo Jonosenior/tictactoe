718ff22d1659bb2a684ce9b0ff2a27cb450f201e
jonathan senior <jonosenior@gmail.com>
2017-12-15 11:11:55 +0100

    Organise methods of main file into understandable super-methods

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -4,27 +4,39 @@ require_relative 'player'
 
 def start_game
   puts "Starting game...\n...\n...\n"
-  game = Game.new
-  board = game.board
-  game.introduction
-  game.ask_player_1_name
-  game.ask_player_2_name
-  game.update_players_array
-  game.print_instructions
-  game.set_current_player
+  @game = Game.new
+  @board = @game.board
+  @game.introduction
+  @game.ask_player_1_name
+  @game.ask_player_2_name
+  @game.update_players_array
+  @game.print_instructions
+  @game.set_current_player
+end
+
+def play_game
   loop do
-    board.display_board
-    game.ask_user_for_move
-    game.verify_input
-    board.place_move(game.current_player.marker, game.move.to_i)
-    if board.is_it_a_draw? || board.any_winning_lines?(game.current_player)
-      board.display_board
+    @board.display_board
+    @game.ask_user_for_move
+    @game.verify_input
+    @board.place_move(@game.current_player.marker, @game.move.to_i)
+    if @board.is_it_a_draw? || @board.any_winning_lines?(@game.current_player)
+      @board.display_board
       break
     end
-    game.change_current_player
+    @game.change_current_player
   end
-  board.any_winning_lines?(game.current_player) ? game.we_have_a_winner : game.everyone_loses
-  game.user_wants_new_game? ? start_game : game.goodbye
+end
+
+def game_over
+  @board.any_winning_lines?(@game.current_player) ? @game.we_have_a_winner : @game.everyone_loses
+end
+
+def play_again?
+  @game.user_wants_new_game? ? start_game : @game.goodbye
 end
 
 start_game
+play_game
+game_over
+play_again?
